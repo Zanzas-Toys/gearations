0b8c80847d6814d347f5f367b991d5b272c1e60a
adrian <aherbez@gmail.com>
2024-03-07 22:52:47 -0800

    working on base

--- gearations.py
+++ gearations.py
@@ -6,36 +6,17 @@ toTip = 3.6 - 1.3
 baseWidth = 3
 gapWidth = 3.16
 gapHeight = 1.6
-gearThickness = 3.2 # 5.2
+
+shaftRad = 5
+pinRad = 2
+pinHeight = 3.2
+clearance = 0.4
+
+gearThickness = 5.2
 
 bw2 = baseWidth / 2
 tr2 = tipDia/2
 gw2 = gapWidth/2
-offsetX = (baseWidth + gapWidth) * -0.5
-
-
-result = (
-    cq.Sketch()
-    .segment((0.0, 0), (0.0, 2.0))
-    .segment((2.0, 0))
-    .close()
-    .arc((0.6, 0.6), 0.4, 0.0, 360.0)
-    .assemble(tag="face")
-    .edges("%LINE", tag="face")
-    .vertices()
-    .chamfer(0.2)
-)
-
-tooth = (
-    cq.Workplane("XZ")
-    #.center(offsetX,0)
-    .moveTo(-bw2, 0)
-    .lineTo(-tr2, toTip-tr2)
-    .threePointArc((0,toTip),(tr2,toTip-tr2))
-    .lineTo(tr2,0)
-    .threePointArc((bw2+gw2,-gapHeight),(bw2+gapWidth,0))
-    .close()
-)
 
 def polarPoint(t, r):
     return (math.cos(t) * r, math.sin(t) * r)
@@ -92,18 +73,54 @@ def gear(numTeeth):
         t = (math.pi * 2) / numTeeth
         gear = simpleTooth(gear, t * (i+1), gearRad, toothTheta, taperTheta)
     gear = gear.close()
-    
-    
-    
     gear = gear.extrude(gearThickness)
     
+    shaft = cq.Workplane("XY").circle(shaftRad + clearance).extrude(10)
     
+    gear = gear.cut(shaft)
     
     return gear
 
-g = gear(20)
-show_object(g)
+baseHeight = 6.3
+magnetHeight = 3
+magnetDia = 25
 
+def base():
+    base = cq.Workplane("XY")
+    base = base.circle(16).extrude(baseHeight-1)
+    
+    shaft = cq.Workplane("XY")
+    base = base.circle(shaftRad).extrude((baseHeight -1)/2 + gearThickness + 0.4)
+    
+    magnet = cq.Workplane("XY")
+    magnet = magnet.circle(magnetDia/2 + clearance).extrude(magnetHeight)
+    
+    
+    
+    #show_object(magnet)
+    
+    base = base.cut(magnet)
+    
+    return base
+
+def cap():
+    cap = cq.Workplane("XY")
+    cap = cap.circle(shaftRad * 1.5).extrude(1.6)
+    cap = cap.edges(">Z or <Z").fillet(0.4)
+    cap = cap.circle(pinRad).extrude(-0.8 - pinHeight)
+    return cap
+
+b = base()
+b = b.translate((0,0,1))
+#show_object(b)
+
+g = gear(20)
+g = g.translate((0,0,baseHeight))
+#show_object(g)
+
+c = cap()
+c = c.translate((0,0,0))
+show_object(c)
 
 # cq.exporters.export(g, "gear_20.stl")
 # cq.exporters.export(gear(10), "gear_10.stl")
